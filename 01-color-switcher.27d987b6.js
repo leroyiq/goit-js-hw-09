ba905527f224eec8f1771f0627cf253eb564b29f
leroyiq <47483075+leroyiq@users.noreply.github.com>
2022-11-16 13:33:21 +0000

    Deploying to gh-pages from @ leroyiq/goit-js-hw-09@70e504d7bb29c8b76b7d7cc24acce0e73936d2e4 🚀

---
+++ 01-color-switcher.27d987b6.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");t.addEventListener("click",(()=>{timerId=setInterval((()=>{d.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.setAttribute("disabled","disabled"),e.removeAttribute("disabled")}),1e3)})),e.addEventListener("click",(()=>{clearInterval(timerId),t.removeAttribute("disabled"),e.setAttribute("disabled","disabled")}));
+//# sourceMappingURL=01-color-switcher.27d987b6.js.map
