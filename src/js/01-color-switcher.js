70e504d7bb29c8b76b7d7cc24acce0e73936d2e4
leroyiq <leroyiq6@gmail.com>
2022-11-16 15:32:56 +0200

    task01 complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyId = document.querySelector('body');
+let timeId = null;
 
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    getRandomHexColor();
+    startBtn.setAttribute('disabled', 'disabled');
+    stopBtn.removeAttribute('disabled');
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'disabled');
+});
+
+function getRandomHexColor() {
+  return (bodyId.style.background = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`);
+}
