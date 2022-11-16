d4b83b712fb5839b801e58549431004a4c417edd
leroyiq <leroyiq6@gmail.com>
2022-11-16 19:22:22 +0200

    task03 complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onStart);
+
+function onStart(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  const delayRef = Number(delay.value);
+  const stepRef = Number(step.value);
+  const amountRef = Number(amount.value);
+  for (let position = 1; position <= amountRef; position += 1) {
+    const delay = delayRef + stepRef * (position - 1);
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
